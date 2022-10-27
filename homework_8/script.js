7145da60ff35bb8dce661d31541cc251b10f104e
Bustrova Viktoriia <Yalla84@gmail.com>
2022-10-27 11:59:06 +0300

    homework_8

--- homework_8/script.js
+++ homework_8/script.js
@@ -5,25 +5,26 @@ const button = document.getElementById('expression');
 button.addEventListener('click', function () {
     
 let result;
-const num1 = Number(document.getElementById('num1').value);
-const num2 = Number(document.getElementById('num2').value);
+const num1 = document.getElementById('num1').value;
+const num2 = document.getElementById('num2').value;
 const operator = document.getElementById('operator').value;
+console.log(num1, num2,)
 
         switch(operator) {
             case '':
                 result = 'Знак не введен';
                 break;
             case '+':
-                result = num1 + num2;
+                result = Number(num1) + Number(num2);
                 break;
             case '-':
-                result = num1 - num2;
+                result = Number(num1) - Number(num2);
                 break;
             case '/':
-                result = num1 / num2;
+                result = Number(num1) / Number(num2);
                 break;
             case '*':
-                result = num1 * num2;
+                result = Number(num1) * Number(num2);
                 break; 
             default: 
                 result = 'Программа не поддерживает такую операцию';
@@ -33,7 +34,7 @@ const operator = document.getElementById('operator').value;
 if (num1 == '') {result = 'Первое число не указано';};
 if (num2 == '') {result = 'Второе число не указано';};
 if (isNaN(num1) || isNaN(num2)) {result = 'Некорретный ввод чисел';};
-//if ((num2 == 0) && (operator == '/')) {result = 'Операция некорректна';};
+if (result === Infinity) {result = 'Операция некорректна';};
 
 console.log(result)
 document.getElementById('result').innerHTML = result;
